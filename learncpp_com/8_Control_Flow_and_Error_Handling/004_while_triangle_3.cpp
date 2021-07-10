3729bf71f8cfdc8507b89cc837e4a9b133f97bca
Anushik Hovhannisyan <anushikhov@gmail.com>
2021-07-10 12:53:31 +0400

    Number, spaces triangle printing with while

---
+++ learncpp_com/8_Control_Flow_and_Error_Handling/004_while_triangle_3.cpp
@@ -0,0 +1,36 @@
+// Print the following:
+//         1
+//       2 1
+//     3 2 1
+//   4 3 2 1
+// 5 4 3 2 1
+
+
+#include <iostream>
+
+int main()
+{
+	int outer{ 1 };
+	while( outer <= 5 )
+	{
+		int inner{ 5 };
+		while( inner >= 1 )
+		{
+			if(inner <= outer)
+			{
+				std::cout << inner << ' ';
+			}
+			else
+			{
+				std::cout << "  ";
+			}
+
+			--inner;
+		}
+
+		std::cout << '\n';
+		++outer;
+	}
+
+	return 0;
+}
