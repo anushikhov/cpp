449bc2a97bbff2fce309c3a84a0ce30277f03f64
Anushik Hovhannisyan <anushikhov@gmail.com>
2021-07-18 18:07:52 +0400

    Index of the max

---
+++ learncpp_com/11_Arrays_Strings_Pointers_References/002_max_index.cpp
@@ -0,0 +1,23 @@
+// Modify the program so that instead of having maxScore hold the largest score directly, a variable named maxIndex hold the index of the largest score.
+
+#include <iostream>
+#include <iterator>
+
+int main()
+{
+	constexpr int scores[]{ 84, 92, 76, 81, 56 };
+
+	int maxIndex{ 0 };
+
+	for(int student{ 0 }; student < static_cast<int>(std::size(scores)); ++ student)
+	{
+		if(scores[student] > scores[maxIndex])
+		{
+			maxIndex = student;
+		}
+	}
+
+	std::cout << "The best score was at " << maxIndex << '\n';
+
+	return 0;
+}
